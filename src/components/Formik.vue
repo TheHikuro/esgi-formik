dd4aec3254fc6ffa9b01493ecf66a100b09f430f
Loan CLERIS <loan.cleris@gmail.com>
2022-12-14 10:57:03 +0100

    init component

---
+++ src/components/Formik.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { defineProps, defineEmits, defineExpose } from 'vue'
+defineProps({
+    initialValues: {
+        type: Object,
+        required: true
+    },
+    validator: {
+        type: Function,
+        required: true
+    },
+    onSubmit: {
+        type: Function,
+        required: true
+    }
+})
+</script>
+
+<template>
+    <slot>
+
+    </slot>
+
+</template>
+
+<style scoped>
+
+</style>
