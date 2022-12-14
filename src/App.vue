bbdfb478ce1ba0dc03840356cb0cae5d1bd06f32
tom.cuillandre@sfr.fr <cuillandret@culligan.fr>
2022-12-14 11:19:49 +0100

    Add cutom input into App
    - InputCustom not already ini

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <script setup>
+import Formik from './components/Formik.vue'
+import InputCustom from './components/InputCustom.vue';
 </script>
 
 <template>
 
   <main>
     <div>
-      yo
+      <Formik>
+        <InputCustom as="select" name="gender">
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+        </InputCustom>
+      </Formik>
     </div>
   </main>
 </template>
